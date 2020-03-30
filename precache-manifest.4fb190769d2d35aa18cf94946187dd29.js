dec93193c0a6e0d40070adec8d6fad85cf0edd7e
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-03-30 16:02:34 +0200

    Updates

---
+++ precache-manifest.4fb190769d2d35aa18cf94946187dd29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f52200373bc102047d72c414818509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d2ee5ea65fe2170330",
+    "url": "/static/css/main.8ff6e584.chunk.css"
+  },
+  {
+    "revision": "5672d2a6d9d635e785b7",
+    "url": "/static/js/2.05bf166d.chunk.js"
+  },
+  {
+    "revision": "15d2ee5ea65fe2170330",
+    "url": "/static/js/main.e0c5c222.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
